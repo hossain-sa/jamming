292b9a47ace025f25fc7973fe9b467178c2c0646
arifhossain-saeed <arifhossain.saeed@hotmail.com>
2025-02-19 23:47:40 +0000

    search bar modified

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,7 +1,24 @@
-function SearchBar(props) {
-	return (
-		<div className="SearchBar">
+import React, { useState } from 'react';
 
+function SearchBar(props) {
+	const {searchPlayList} = props;
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const handleTextChange = (event) => {
+		event.preventDefault();
+		setSearchTerm(event.target.value);
+	}
+
+	const handleSearch = () => {
+		searchPlayList(searchTerm);
+	}
+
+	return (
+		<div className="search-bar">
+			<input type="text" placeholder="Search..." onChange={handleTextChange} />
+			<button className="search-button" onClick={handleSearch}>
+				Search
+			</button>
 		</div>
 	)
 }
