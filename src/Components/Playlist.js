6f5a6b0ed926e90edeadb309f4269770c05a10fa
arifhossain-saeed <arifhossain.saeed@hotmail.com>
2025-02-19 23:15:04 +0000

    playlist setup

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -1,9 +1,11 @@
 import TrackList from './TrackList';
 
 function Playlist(props) {
+	const {playListTracks, removeTrack} = props;
+
 	return (
 		<div className="Playlist">
-
+			<TrackList tracks={playListTracks} removeTrack={removeTrack} />
 		</div>
 	)
 }
